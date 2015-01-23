3f13aaa38f8e86b951989e4e1a36c884ffeae708
Arthur Igberdin <art.igberdin@gmail.com>
2015-01-23 22:59:19 +0300

    Setup project

---
+++ rg-ios-base/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  rg-ios-base
+//
+//  Created by Artur on 23/01/15.
+//  Copyright (c) 2015 Artur Igberdin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
